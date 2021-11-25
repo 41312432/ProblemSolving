4375cd52964c5d5c4c087f609ffd47a93a91895f
Wansik Jo <jws5327@ajou.ac.kr>
2021-11-25 15:03:36 +0900

    BOJ: Complete step-6 function
    BOJ 6th step : function
    https://www.acmicpc.net/step/5
    
    1	15596	정수 N개의 합
    2	4673	셀프 넘버
    	// time complexity -> O(n)
    	d(n)을 정의하고, 1부터 10000까지의 숫자에 대해서 숫자마다 그 숫자의 전 숫자를 순착적으로 탐색하여 생성자인경우 루프를 중지, 끝까지 돌았는데 생성자가 없는 경우 바로 프린트 해 주었다.
    	이 경우 O(n^2) time out이 난다.
    3	1065	한수

---
+++ BOJ/step_by_step/6_function/1065.py
@@ -0,0 +1,27 @@
+'''
+문제
+어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다. 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다. N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오. 
+
+입력
+첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
+
+출력
+첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
+'''
+
+def is_HanSoo(n):
+    if n<100:
+        return True
+    numbers = []
+    while n >= 1:
+        numbers.append(n%10)
+        n = n // 10
+    if numbers[0] + numbers[-1] == numbers[1] * 2:
+        return True
+
+N = int(input())
+result = 0
+for i in range(1, N+1):
+    if is_HanSoo(i):
+        result += 1
+print(result)
