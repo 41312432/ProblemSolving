3e1a70bda08dd94d47431a7effd6c260c9d842de
Wansik Jo <jws5327@ajou.ac.kr>
2021-12-06 20:10:35 +0900

    BOJ: Complete step-11 sorting
    BOJ 11th step : sorting
    https://www.acmicpc.net/step/9
    
    1	2750	 수 정렬하기
    2	2751	 수 정렬하기 2	*
    			//sys.stdin / sys.stdout
    3	10989	 수 정렬하기 3	***
    			//counting sort
    			//#https://www.cs.miami.edu/home/burt/learning/Csc517.091/workbook/countingsort.html
    4	2108	 통계학
    5	1427	 소트인사이드
    6	11650	 좌표 정렬하기
    7	11651	 좌표 정렬하기 2
    8	1181	 	단어 정렬
    9	10814	 나이순 정렬
    10	18870	 좌표 압축	*

---
+++ BOJ/step_by_step/12_sorting/18870.py
@@ -0,0 +1,29 @@
+'''
+문제
+수직선 위에 N개의 좌표 X1, X2, ..., XN이 있다. 이 좌표에 좌표 압축을 적용하려고 한다.
+
+Xi를 좌표 압축한 결과 X'i의 값은 Xi > Xj를 만족하는 서로 다른 좌표의 개수와 같아야 한다.
+
+X1, X2, ..., XN에 좌표 압축을 적용한 결과 X'1, X'2, ..., X'N를 출력해보자.
+
+입력
+첫째 줄에 N이 주어진다.
+
+둘째 줄에는 공백 한 칸으로 구분된 X1, X2, ..., XN이 주어진다.
+
+출력
+첫째 줄에 X'1, X'2, ..., X'N을 공백 한 칸으로 구분해서 출력한다.
+'''
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+coords = list(map(int, input().split()))
+coords_set = list(sorted(set(coords)))
+
+coords_dic = {coords_set[i]:i for i in range(len(coords_set))}
+
+for coord in coords:
+    print(coords_dic[coord])
