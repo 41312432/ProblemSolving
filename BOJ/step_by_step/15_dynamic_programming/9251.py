f0d834a0fc1d915bbb4960f3f00e2e07cb18156d
Wansik Jo <jws5327@ajou.ac.kr>
2021-12-17 23:57:29 +0900

    BOJ: Complete step-15 dynamic-programming - 1
    BOJ 15th step : dp
    https://www.acmicpc.net/step/16
    
    1 1003 피보나치 함수 *
    2 9184 신나는 함수 실행
    3 1904 01타일 **
    //https://sungmin-joo.tistory.com/21?category=851625
    4 9461 파도반 수열
    5 1149 RGB거리 **
    6 1932 정수 삼각형
    7 2579 계단 오르기 ************
    //https://sungmin-joo.tistory.com/18
    8 1463 1로 만들기 **
    //https://hongcoding.tistory.com/46
    9 10844 쉬운 계단 수 **
    //https://pacific-ocean.tistory.com/151
    10 2156 포도주 시식 **
    //https://pacific-ocean.tistory.com/152
    11 11053 가장 긴 증가하는 부분 수열	 ****
    //https://pacific-ocean.tistory.com/153
    12 11054 가장 긴 바이토닉 부분 수열 ****
    //https://pacific-ocean.tistory.com/158
    13 2565 전깃줄 *
    14 9251 LCS	******
    15 1912 연속합
    16 12865 평범한 배낭	*********

---
+++ BOJ/step_by_step/15_dynamic_programming/9251.py
@@ -0,0 +1,32 @@
+'''
+LCS 
+ 
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	43133	17901	13026	40.554%
+문제
+LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열이 주어졌을 때, 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
+
+예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
+
+입력
+첫째 줄과 둘째 줄에 두 문자열이 주어진다. 문자열은 알파벳 대문자로만 이루어져 있으며, 최대 1000글자로 이루어져 있다.
+
+출력
+첫째 줄에 입력으로 주어진 두 문자열의 LCS의 길이를 출력한다.
+'''
+
+import sys
+
+s1 = sys.stdin.readline().strip().upper() 
+s2 = sys.stdin.readline().strip().upper()
+
+dp = [[0]*(len(s2)+1) for _ in range(len(s1)+1)]
+
+for i in range(1, len(s1)+1):
+    for j in range(1, len(s2)+1):
+        if s1[i-1] == s2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[-1][-1])
